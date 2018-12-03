d29bc7e99530f99e028b63a0720a3a6d98bb9f76
扶冬冬 <fudongdong@meituan.com>
2018-12-03 15:33:59 +0800

    pod打包静态库

---
+++ FSGesture-0.0.1/build/Pods.build/Release-iphonesimulator/FSGesture.build/Objects-normal/i386/FSGesture-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /var/folders/_v/qc1gj9bj0dg4w6snwg5nc4_r0000gp/T/cocoapods-mvs3jrt7/Pods/Target\ Support\ Files/FSGesture/FSGesture-dummy.m \
+  /var/folders/_v/qc1gj9bj0dg4w6snwg5nc4_r0000gp/T/cocoapods-mvs3jrt7/Pods/Target\ Support\ Files/FSGesture/FSGesture-prefix.pch
---
+++ FSGesture-0.0.1/build/Pods.build/Release-iphonesimulator/FSGesture.build/Objects-normal/x86_64/FSCircleLayer.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /var/folders/_v/qc1gj9bj0dg4w6snwg5nc4_r0000gp/T/cocoapods-mvs3jrt7/Pods/FSGesture/Classes/FSCircleLayer.m \
+  /var/folders/_v/qc1gj9bj0dg4w6snwg5nc4_r0000gp/T/cocoapods-mvs3jrt7/Pods/Target\ Support\ Files/FSGesture/FSGesture-prefix.pch \
+  /var/folders/_v/qc1gj9bj0dg4w6snwg5nc4_r0000gp/T/cocoapods-mvs3jrt7/Pods/FSGesture/Classes/FSCircleLayer.h
